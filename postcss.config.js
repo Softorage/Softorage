cf87353b54bc7b5bc75ba7b914dbbfe00247d388
SanmayJoshi <sanmaycjoshi@gmail.com>
2020-10-05 23:16:01 +0530

    Updated PostCSS config to incorporate missing Bootstrap elements

--- postcss.config.js
+++ postcss.config.js
@@ -5,10 +5,13 @@ module.exports = {
   plugins: {
     "@fullhuman/postcss-purgecss": {
       content: ['./hugo_stats.json'],
+      whitelist: ["alert", "close", "collapse", "collapsing", "fade", "modal", "popover", "tooltip", "blockquote"], //see https://github.com/uncss/uncss/wiki/Using-uncss-with-Bootstrap
+      whitelistPatterns: [/alert$/, /close$/, /collapse$/, /fade$/, /modal$/, /popover$/, /tooltip$/, /show/, /alert-.*/, /close-.*/, /collapse-.*/, /fade-.*/, /modal-.*/, /popover-.*/, /tooltip-.*/],
+      whitelistPatternsChildren: [/alert$/, /close$/, /collaps$/, /fade$/, /modal$/, /popover$/, /tooltip$/, /alert-.*/, /close-.*/, /collapse-.*/, /fade-.*/, /modal-.*/, /popover-.*/, /tooltip-.*/],
       defaultExtractor: (content) => {
         let els = JSON.parse(content).htmlElements;
         return els.tags.concat(els.classes, els.ids);
-    }
+      }
     },
     autoprefixer: {},
     cssnano: {
