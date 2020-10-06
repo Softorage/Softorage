ec49a862b52dc1dad7f01c80c79e0ad1367a3f35
SanmayJoshi <sanmaycjoshi@gmail.com>
2020-10-06 09:03:23 +0530

    Reverting from PostProcess, since it can't recognise classnames from JS

--- postcss.config.js
+++ postcss.config.js
@@ -4,14 +4,10 @@ browserslist: [
 module.exports = {
   plugins: {
     "@fullhuman/postcss-purgecss": {
-      content: ['./hugo_stats.json'],
+      content: ["layouts/**/*.html"],
       whitelist: ["alert", "close", "collapse", "collapsing", "fade", "modal", "popover", "tooltip", "blockquote"], //see https://github.com/uncss/uncss/wiki/Using-uncss-with-Bootstrap
       whitelistPatterns: [/alert$/, /close$/, /collapse$/, /fade$/, /modal$/, /popover$/, /tooltip$/, /show/, /alert-.*/, /close-.*/, /collapse-.*/, /fade-.*/, /modal-.*/, /popover-.*/, /tooltip-.*/],
-      whitelistPatternsChildren: [/alert$/, /close$/, /collaps$/, /fade$/, /modal$/, /popover$/, /tooltip$/, /alert-.*/, /close-.*/, /collapse-.*/, /fade-.*/, /modal-.*/, /popover-.*/, /tooltip-.*/],
-      defaultExtractor: (content) => {
-        let els = JSON.parse(content).htmlElements;
-        return els.tags.concat(els.classes, els.ids);
-      }
+      whitelistPatternsChildren: [/alert$/, /close$/, /collaps$/, /fade$/, /modal$/, /popover$/, /tooltip$/, /alert-.*/, /close-.*/, /collapse-.*/, /fade-.*/, /modal-.*/, /popover-.*/, /tooltip-.*/]
     },
     autoprefixer: {},
     cssnano: {
